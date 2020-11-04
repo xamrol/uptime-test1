f3e2286a5ea16d24bd9a10ea68db72918877599e
Arol Gbeto-fia <g.senami94@gmail.com>
2020-11-03 19:39:45 -0500

    Commit 2.4

--- batch.bat
+++ batch.bat
@@ -1,11 +1,13 @@
+cmd /c
+
 "C:\Program Files\CodeBlocks\MinGW\bin"\g++.exe -c calculator.cpp
 
 "C:\Program Files\CodeBlocks\MinGW\bin"\g++.exe -c main.cpp
 
 "C:\Program Files\CodeBlocks\MinGW\bin"\g++.exe -o test.exe main.o calculator.o
 
-start test.exe
+test.exe
 
 "C:\Program Files\Cppcheck"\cppcheck.exe --verbose --enable=all --enable=style --xml --output-file=cppCheckResults.xml .
 
-cmd /c start CppCheckResults.xml
+start CppCheckResults.xml
