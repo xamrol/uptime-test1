39807b7d0a70d24a1ace59f52277c1da8823be0c
Arol Gbeto-fia <g.senami94@gmail.com>
2020-11-04 21:21:56 -0500

    Commit 2.x

--- batch.bat
+++ batch.bat
@@ -1,9 +1,9 @@
-"C:\Program Files\CodeBlocks\MinGW\bin\"g++.exe -Wall -fexceptions -g  -c calculator.cpp main.cpp
-
-"C:\Program Files\CodeBlocks\MinGW\bin\"g++.exe -o test.exe calculator.o main.o
-
-test.exe
-
-"C:\Program Files\Cppcheck\"cppcheck.exe --verbose --enable=all --enable=style --xml --output-file=cppCheckResults.xml .
-
-start CppCheckResults.xml
+::Test If script has Admin Priviledges/is elevated
+net session >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+ ECHO you are Administrator
+) ELSE (
+ ECHO you are NOT Administrator. Exiting...
+ PING 127.0.0.1 > NUL 2>&1
+ EXIT /B 1
+)
