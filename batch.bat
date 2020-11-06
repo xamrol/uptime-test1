9f92fd242382f7f64ae072de44721d3bfb84dae6
Arol Gbeto-fia <g.senami94@gmail.com>
2020-11-06 14:59:28 -0500

    Commit 5.1

--- batch.bat
+++ batch.bat
@@ -3,7 +3,7 @@
 "C:\MinGW\bin\g++.exe" -c test_unit.cpp
 "C:\MinGW\bin\g++.exe" -o uptime-app calculator.o main.o
 uptime-app.exe
-"C:\MinGW\bin\g++.exe" -o uptime-test calculator.o test_unit.o
+"C:\MinGW\bin\g++.exe" -o uptime-test test_unit.o calculator.o
 uptime-test.exe
 "C:\Program Files\Cppcheck\cppcheck.exe" --check-config --xml --output-file=cppCheckResults.xml --verbose .
 start CppCheckResults.xml
